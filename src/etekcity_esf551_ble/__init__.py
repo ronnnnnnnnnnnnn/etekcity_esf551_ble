d4c7ab749c8360fd59eb340cb8b6f207e2aa82fd
Ron <ron.benyosef@outlook.com>
2025-11-16 19:37:01 +0000

    Cleanup __init__.py

--- src/etekcity_esf551_ble/__init__.py
+++ src/etekcity_esf551_ble/__init__.py
@@ -2,16 +2,9 @@ from ._version import __version__, __version_info__
 from .esf551 import ESF551Scale, EtekcitySmartFitnessScaleWithBodyMetrics
 from .esf551.body_metrics import BodyMetrics, Sex
 from .const import (
-    ALIRO_CHARACTERISTIC_UUID,
     DISPLAY_UNIT_KEY,
     IMPEDANCE_KEY,
-    WEIGHT_CHARACTERISTIC_UUID_NOTIFY,
     WEIGHT_KEY,
-)
-from .esf551.const import (
-    HW_REVISION_STRING_CHARACTERISTIC_UUID,
-    SW_REVISION_STRING_CHARACTERISTIC_UUID,
-    UNIT_UPDATE_COMMAND,
 )
 from .esf24 import ESF24Scale
 from .parser import (
@@ -19,9 +12,6 @@ from .parser import (
     ScaleData,
     WeightUnit,
 )
-
-# Backward compatibility alias
-EtekcitySmartFitnessScale = ESF551Scale
 
 __all__ = [
     "EtekcitySmartFitnessScale",
@@ -35,11 +25,5 @@ __all__ = [
     "BodyMetrics",
     "Sex",
     "BluetoothScanningMode",
-    # Backward compatibility - ESF-551 constants
-    "ALIRO_CHARACTERISTIC_UUID",
     "DISPLAY_UNIT_KEY",
-    "HW_REVISION_STRING_CHARACTERISTIC_UUID",
-    "SW_REVISION_STRING_CHARACTERISTIC_UUID",
-    "UNIT_UPDATE_COMMAND",
-    "WEIGHT_CHARACTERISTIC_UUID_NOTIFY",
 ]
