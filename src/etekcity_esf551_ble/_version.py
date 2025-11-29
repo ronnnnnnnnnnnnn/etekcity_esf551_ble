c8c1f9f04a51cd2b627886001c7a20c5837ad7f7
Ron <ron.benyosef@outlook.com>
2025-11-29 16:30:55 +0000

    Add version parsing for post-release versions in _version.py

--- src/etekcity_esf551_ble/_version.py
+++ src/etekcity_esf551_ble/_version.py
@@ -1,2 +1,14 @@
 __version__ = "0.3.2"
-__version_info__ = tuple(map(int, __version__.split(".")))
+
+def _parse_version_info(version_str: str) -> tuple:
+    """Parse version string into tuple of integers, handling post-release versions."""
+    import re
+    # Remove version suffixes (post, dev, alpha, beta, rc, etc.) and local version identifiers
+    # Examples: "0.3.2.post1" -> "0.3.2", "1.2.3.dev0" -> "1.2.3", "2.0.0+local" -> "2.0.0"
+    base_version = re.sub(r'\.(post|dev|a|alpha|b|beta|rc|c|pre|preview)\d+.*$', '', version_str)
+    base_version = base_version.split('+')[0]  # Remove local version identifier
+    # Split by '.' and convert to integers
+    parts = base_version.split('.')
+    return tuple(map(int, parts))
+
+__version_info__ = _parse_version_info(__version__)
